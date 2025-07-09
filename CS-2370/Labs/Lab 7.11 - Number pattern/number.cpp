8a8f61fed8676feeb740aed5bcf7a9b9cd61231b
Brandon Reich <brandon.reich@netdocuments.com>
2025-07-09 13:09:38 -0600

    add lab 7.11

---
+++ CS-2370/Labs/Lab 7.11 - Number pattern/number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void PrintNumPattern(int num1, int num2) {
+    if (num1 < 0) {
+        cout << num1 << " ";
+        return;
+    }
+
+    cout << num1 << " ";
+    PrintNumPattern(num1 - num2, num2);
+    cout << num1 << " ";
+}
+
+int main() {
+   int num1;
+   int num2;
+
+   cin >> num1;
+   cin >> num2;
+   PrintNumPattern(num1, num2);
+   
+   return 0;
+}
