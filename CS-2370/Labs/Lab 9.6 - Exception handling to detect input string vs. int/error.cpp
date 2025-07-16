61a0431e204383cd4dede5d6ee5fc3b4ce4a2fa9
Brandon Reich <brandon.reich@netdocuments.com>
2025-07-16 13:41:18 -0600

    fix lab 9.6

--- CS-2370/Labs/Lab 9.6 - Exception handling to detect input string vs. int/error.cpp
+++ CS-2370/Labs/Lab 9.6 - Exception handling to detect input string vs. int/error.cpp
@@ -1,22 +1,34 @@
-The given program reads a list of single-word first names and ages (ending with -1), and outputs that list with the age incremented. The program fails and throws an exception if the second input on a line is a string rather than an int. At FIXME in the code, add a try/catch statement to catch ios_base::failure, and output 0 for the age.
+#include <string>
+#include <iostream>
 
-Ex: If the input is:
+using namespace std;
 
-Lee 18
-Lua 21
-Mary Beth 19
-Stu 33
--1
-then the output is:
+int main() {
+   string inputName;
+   int age;
+   // Set exception mask for cin stream
+   cin.exceptions(ios::failbit);
 
-Lee 19
-Lua 22
-Mary 0
-Stu 34
-Note: Insert the following code in the catch block to clear the failbit and cin buffer so a new input can be read correctly:
+   cin >> inputName;
+   while(inputName != "-1") {
+      // FIXME: The following line will throw an ios_base::failure.
+      //        Insert a try/catch statement to catch the exception.
+      //        Clear cin's failbit to put cin in a useable state.
 
-// Clear failbit to be able to use cin again
-cin.clear();
-// Throw away the rest of the failed input line from cin buffer
-string garbage;
-getline(cin, garbage);
+      try {
+        cin >> age;
+        cout << inputName << " " << (age + 1) << endl;
+      }
+      catch (ios_base::failure& e) {
+        cout << inputName << " 0" << endl;
+        cin.clear();
+
+        string garbage;
+        getline(cin, garbage);
+      }
+
+      cin >> inputName;
+   }
+   
+   return 0;
+}
